39c2db034d03ee51b653e2707aae38c0f5e5f606
caiqueluci0 <caique.lucio@sptech.school>
2024-01-18 21:05:32 -0300

    Corrigindo erros de merge

--- site/public/index.php
+++ site/public/index.php
@@ -1,20 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-<<<<<<< HEAD
     <link rel="stylesheet" href="css/style.css" >
-</head>
-<body>
-    <?php 
-    include('html\header.html');    
-    ?>
-</body>
-</html>
-=======
-    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <?php 
@@ -26,4 +16,4 @@
     ?>
 </body>
 </html>
->>>>>>> 2b488f72e2bf87cff20f72330573368b30cf069b
+
