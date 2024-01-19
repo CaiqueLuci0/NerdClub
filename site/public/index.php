0222c5225e1e0a4bb6764939a01c80847d457caa
Nicollas200 <nicollasbispopereira12@gmail.com>
2024-01-18 23:56:54 -0300

    Atualizando header e banner

--- site/public/index.php
+++ site/public/index.php
@@ -1,19 +1,37 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="css/style.css" >
+    <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
-    <?php 
-    include("html\header.html")
-    ?>
+    <section> <?php
+                include("html\header.html")
+                ?>
+        <div class="banner">
+            <div class="banner-p">
+                <div class="banner-a"><img src="assets/img/logo.png ">
+                    <h1>Me diz seu personagem favorito?</h1> <button><a href="">Jogar</a></button>
+                </div>
+                <div></div>
+
+            </div>
+            <div class="banner-p">
+                <div></div>
+                <div></div>
+
+            </div>
+        </div>
+    </section>
 
     <?php
     include("leaderboard.php")
     ?>
-</body>
-</html>
 
+</body>
+
+</html>
